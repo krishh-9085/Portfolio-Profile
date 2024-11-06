b9390c05f4a8d32f87bd4b28b3d925f6efbb03a4
Krishna Rohilla <126801156+krishh-9085@users.noreply.github.com>
2024-11-06 20:37:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,31 +5,41 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+document.body.classList.add("dark-button"); // Start in dark mode
+
 var moon = document.getElementById("moon");
+var kr = document.getElementById("kr");
+moon.src = "assets/SUN.svg"; // Set the moon icon to sun initially
+
 moon.onclick = function () {
   document.body.classList.toggle("dark-button");
   if (document.body.classList.contains("dark-button")) {
-    moon.src = "assets/SUN.svg";
+    moon.src = "assets/SUN.svg"; // Keep sun icon when in dark mode
+    kr.src = "assets/logo_dark.png"; // Use dark logo
   } else {
-    moon.src = "assets/MOON.svg";
+    moon.src = "assets/MOON.svg"; // Change to moon icon when switching to light mode
+    kr.src = "assets/fevicon.png"; // Use light logo
   }
 };
 
-var mo = document.getElementById("mo");
-mo.onclick = function () {
-  document.body.classList.toggle("dark-button");
+var sun = document.getElementById("sun");
+var kr_ham = document.getElementById("kr_ham");
+sun.onclick = function () {
+  document.body.classList.toggle("dark-button"); // Switch to light mode
+  
   if (document.body.classList.contains("dark-button")) {
-    mo.src = "assets/SUN.svg";
-    
+    sun.src = "assets/SUN.svg";
+    kr_ham.src = "assets/logo_dark.png";
   } else {
-    mo.src = "assets/MOON.svg";
+    sun.src = "assets/MOON.svg";
+    kr_ham.src = "assets/fevicon.png";
   }
 };
 
 const toggleDarkMode = () => {
-    document.body.classList.toggle("dark-button");
-    const icons = document.querySelectorAll(".social-container .icon");
-    icons.forEach(icon => {
-      icon.style.filter = document.body.classList.contains("dark-button") ? "invert(1)" : "invert(0)";
-    });
-  }
+  document.body.classList.toggle("dark-button");
+  const icons = document.querySelectorAll(".social-container .icon");
+  icons.forEach(icon => {
+    icon.style.filter = document.body.classList.contains("dark-button") ? "invert(1)" : "invert(0)";
+  });
+};
